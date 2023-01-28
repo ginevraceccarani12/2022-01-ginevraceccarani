72eb00036c00245d681a67b757f849b6ea0020a5
ginevraceccarani12 <113507772+ginevraceccarani12@users.noreply.github.com>
2023-01-28 15:40:27 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,44 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  angleMode(DEGREES);
 }
-
 function draw() {
-  // put drawing code here
-  background("red");
+  background("black");
+  strokeWeight (1)
+  stroke("white");
+  noFill();
+
+  
+  for (i = 0; i < 10; i++) {
+    const angle = frameCount + i * 5; // angle
+    const k = 200; // amplitude of rotation
+    const rotation = sin(angle) * k; // resulting rotation
+    const side = 20;// initial side
+    
+    push();
+    rectMode(CENTER);
+    translate(width/4 , height/2);
+    rotate(rotation);
+    rect(0, 0, side * i, side * i);
+    pop();
+
+ 
+
+    
+    push();
+    ellipseMode(CORNER);
+    translate(width/2 , height/2);
+    rotate(rotation);
+    ellipse(0, 0, side * i, side * i);
+    pop();
+
+    push();
+    rectMode(CENTER);
+    translate(width - width/4, height/2);
+    rotate(rotation);
+    rect(0, 0, side * i, side * i);
+    pop();
+   
+    
+  }
 }
